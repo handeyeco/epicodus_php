875a29e8fced9159adaa975624eb1ba29d9778f7
Matthew Curtis <matthew@matthewbryancurtis.com>
2016-09-05 14:55:30 -0700

    Finish Objects in Silex

---
+++ oo_php/rectangle/src/Rectangle.php
@@ -0,0 +1,33 @@
+<?php
+
+class Rectangle {
+  private $length;
+  private $width;
+
+  function __construct($length, $width) {
+    $this->length = $length;
+    $this->width = $width;
+  }
+
+  function isSquare() {
+    return $this->length == $this->width;
+  }
+
+  function getArea() {
+    return $this->length * $this->width;
+  }
+
+  function setLength($new_length) {
+    $this->length = (float) $new_length;
+  }
+
+  function getLength() {
+    return $this->length;
+  }
+
+  function setWidth($new_width) {
+    $this->width = (float) $new_width;
+  }
+}
+
+?>
